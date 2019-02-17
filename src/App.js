1bddd81576c922f92c23f732baeca89df241ec31
adamgparsons <adamgeorgeparsons@gmail.com>
2019-02-17 17:22:59 +0000

    added total hours

--- src/App.js
+++ src/App.js
@@ -6,11 +6,53 @@ let defaultStyle = {
   color: defaultTextColor
 };
 
-class Aggregate extends Component {
+let fakeServerData = {
+  user: {
+    name: "Adam",
+    playlists: [
+      {
+        name: "My favourites",
+        songs: [
+          { name: "Beat it", duration: 1345 },
+          { name: "Run baby run", duration: 1825 },
+          { name: "You make me feel like HR", duration: 1825 }
+        ]
+      },
+      {
+        name: "Discover weekly",
+        songs: [
+          { name: "Who rang", duration: 1145 },
+          { name: "Jump out da gym", duration: 1225 },
+          { name: "Fool me thrice", duration: 7000 }
+        ]
+      }
+    ]
+  }
+};
+
+class PlaylistCounter extends Component {
   render() {
     return (
       <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
-        <h2 style={{ color: defaultTextColor }}>Number Text</h2>
+        <h2 style={{ color: defaultTextColor }}>
+          {this.props.playlists.length} playlists
+        </h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs);
+    }, []);
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration;
+    }, 0);
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>{Math.round(totalDuration / 60)} hours</h2>
       </div>
     );
   }
@@ -20,7 +62,7 @@ class Filter extends Component {
   render() {
     return (
       <div style={defaultStyle}>
-        <img />
+        <img alt="stuff" />
         <input type="text" />
       </div>
     );
@@ -31,7 +73,7 @@ class Playlist extends Component {
   render() {
     return (
       <div style={{ ...defaultStyle, width: "25%", display: "inline-block" }}>
-        <img />
+        <img alt="stuff" />
         <h3>Playlist name</h3>
         <ul>
           <li>Song 1</li>
@@ -44,12 +86,31 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {} };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData });
+    }, 1000);
+  }
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
-        <Aggregate />
-        <Aggregate />
+        {this.state.serverData.user ? (
+          <div>
+            <h1 style={{ ...defaultStyle, "font-size": "54px" }}>
+              {this.state.serverData && this.state.serverData.user.name}'s
+              playlists
+            </h1>
+            <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+            <HoursCounter playlists={this.state.serverData.user.playlists} />
+          </div>
+        ) : (
+          <h1 style={defaultStyle}>Loading...</h1>
+        )}
+
         <Filter />
         <Playlist />
         <Playlist />
